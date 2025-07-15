1892fa37a8bd559810c6ce9605c4df4a389884ad
SierraJC <7351311+SierraJC@users.noreply.github.com>
2025-07-15 20:14:29 +1000

    chore(stow): create necessary directories before setting up symlinks

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -16,6 +16,17 @@ warning() {
   tput sgr0
 }
 
+# Directories to create before symlinks are made (to avoid symlinking the whole folder to dotfiles)
+dirs_to_create=(
+  "$HOME/.config/gh"
+  "$HOME/.config/iterm2"
+  "$HOME/.claude"
+)
+
+for dir in "${dirs_to_create[@]}"; do
+  mkdir -p "$dir"
+done
+
 title "📦 Setting up symlinks"
 
 stow --restow stow
