b1a266f6727b9959e0e9a841f73fd6834eb6dd2f
SierraJC <7351311+SierraJC@users.noreply.github.com>
2025-11-21 17:12:12 +1100

    refactor(fish): cleaner caching

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -5,8 +5,7 @@ set -q PAGER; or set -Ux PAGER less
 set -q VISUAL; or set -Ux VISUAL code
 set -q EDITOR; or set -Ux EDITOR nvim
 
-# Set browser on macOS.
-set -q IS_MACOS; and set -Ux BROWSER open
+set -q BROWSER; or set -Ux EDITOR open
 
 # Other vars
 set -q FISH_THEME; or set -U FISH_THEME "Catppuccin Mocha"
---
+++ fish/.config/fish/functions/__cache_and_source.fish
@@ -0,0 +1,11 @@
+function __cache_and_source \
+    --argument-names cache_file cache_cmd
+
+    set -l cache_path $__fish_cache_dir/$cache_file
+
+    if not test -f $cache_path
+        eval $cache_cmd >$cache_path
+    end
+
+    source $cache_path
+end
