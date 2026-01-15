c2a624cc79a277f15692fc30cbe537e47bd31a9e
SierraJC <7351311+SierraJC@users.noreply.github.com>
2026-01-15 20:22:36 +1100

    feat(fish): add magic enter functionality from zsh

---
+++ fish/.config/fish/conf.d/magic-enter.fish
@@ -0,0 +1,27 @@
+function magic-enter-cmd --description "Command to run when pressing enter on an empty prompt"
+    set --local cmd 'ls -laF'
+
+    if command git rev-parse --is-inside-work-tree &>/dev/null
+        set cmd "git status -sb"
+    end
+
+    echo $cmd
+end
+
+function magic-enter
+    set -l cmd (commandline)
+    if test -z "$cmd"
+        commandline -r (magic-enter-cmd)
+        commandline -f suppress-autosuggestion
+    end
+    commandline -f execute
+end
+
+function magic-enter-bindings --description "Bind magic-enter for default and vi key bindings"
+    bind \r magic-enter
+    if functions -q fish_vi_key_bindings
+        bind -M insert \r magic-enter
+        bind -M default \r magic-enter
+    end
+end
+magic-enter-bindings
