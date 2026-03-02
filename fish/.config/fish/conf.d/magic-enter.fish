0dc935bd9d8046692d38a59fa9e4ab8ec11bc4d9
SierraJC <7351311+SierraJC@users.noreply.github.com>
2026-03-02 17:42:03 +1100

    fix(magic-enter): remove buffer shenanigans
    Fixes issue where multi line prompts would cut off magic cmd buffer. Code was too complex for such a dumb feature, so  I have simplified it a lot.

--- fish/.config/fish/conf.d/magic-enter.fish
+++ fish/.config/fish/conf.d/magic-enter.fish
@@ -1,34 +1,23 @@
-function magic-enter-cmd --description "Command to run when pressing enter on an empty prompt"
-    set --local cmd ls -laF
-
-    # if command git rev-parse --is-inside-work-tree &>/dev/null
-    #     set cmd git status -sb
-    # end
-
-    # Output as tokens (one per line) so command substitution returns a proper argv list.
-    printf '%s\n' $cmd
-end
-
-function magic-enter
-    set -l cmd (commandline)
-    if test -z "$cmd"
-        # Run the magic command directly so it doesn't get recorded in history.
-        set -l magic_cmd (magic-enter-cmd)
-        commandline -f suppress-autosuggestion
-        printf '\n'
-        $magic_cmd
-        commandline -f repaint
+function magic-enter --description "Run a default command on empty Enter"
+    if test -n (commandline)
+        commandline -f execute
         return
     end
+
+    set -l cmd 'ls -lahF'
+    #if command git rev-parse --is-inside-work-tree &>/dev/null
+    #   set cmd 'git status -sb'
+    #end
+
+    commandline -r $cmd
+    commandline -f suppress-autosuggestion
     commandline -f execute
 end
 
-function magic-enter-bindings --description "Bind magic-enter for default and vi key bindings"
+function __magic_enter_bind --description "Bind magic-enter to Enter key"
     bind \r magic-enter
-    if functions -q fish_vi_key_bindings
-        bind -M insert \r magic-enter
-        bind -M default \r magic-enter
-    end
+    bind -M insert \r magic-enter
+    bind -M default \r magic-enter
 end
 
-status is-interactive; and magic-enter-bindings
+status is-interactive; and __magic_enter_bind
